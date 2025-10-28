2c618c0cd849003bf77e4ee2ddb61194e8166d39
Camila Alves <82186915+camiladuart@users.noreply.github.com>
2025-10-28 18:09:52 -0300

    Update smoke_test.py

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,1 +1,8 @@
+from coref import CorefDataset
 
+DATA_DIR = r"C:\Users\PC\coref_data\ontonotes"
+
+ds = CorefDataset(DATA_DIR, split="dev")
+print("n_docs:", len(ds))
+print("primeiro doc_key:", ds[0]["doc_key"])
+print("primeira sentença:", ds[0]["sentences"][0][:20])
