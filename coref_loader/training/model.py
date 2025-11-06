c9ced104d6de4d5f699dee92c9725c0b4646120a
Camila Alves <82186915+camiladuart@users.noreply.github.com>
2025-11-05 22:25:11 -0300

    Update model.py

--- coref_loader/training/model.py
+++ coref_loader/training/model.py
@@ -66,34 +66,34 @@ class CorefModel(nn.Module): #nn.Module do torch.nn -> lidar com classes com cam
     
     #tensorflow -> transformers
     def get_prediction_and_loss(
-    self,
-    input_ids: torch.LongTensor,
-    attention_mask: torch.LongTensor,
-    span_starts: torch.LongTensor,
-    span_ends: torch.LongTensor,
-    span_batch_idx: torch.LongTensor,
-    mention_labels: torch.LongTensor = None
+        self,
+        input_ids: torch.LongTensor,
+        attention_mask: torch.LongTensor,
+        span_starts: torch.LongTensor,
+        span_ends: torch.LongTensor,
+        span_batch_idx: torch.LongTensor,
+        mention_labels: torch.LongTensor = None
 ):
-    #padronizando a máscara
-    attention_mask = attention_mask.long()
+        #padronizando a máscara
+        attention_mask = attention_mask.long()
 
-    logits = self.forward(
-        input_ids=input_ids,
-        attention_mask=attention_mask,
-        span_starts=span_starts,
-        span_ends=span_ends,
-        span_batch_idx=span_batch_idx
-    )
+        logits = self.forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            span_starts=span_starts,
+            span_ends=span_ends,
+            span_batch_idx=span_batch_idx
+        )
 
-    #loss=erro (compara verdadeiro com as predictions feitas)
-    loss = None #começa com loss vazia (se não houver rótulos, nao precisa calcular nada)
-    if mention_labels is not None:
-        if mention_labels.numel() == 0:
-            loss = logits.new_tensor(0.0)
-        else:
-            loss = torch.nn.functional.binary_cross_entropy_with_logits(
-                logits, mention_labels.float()
-            ) #binary_cross_entropy_with_logits para comparar logits (notas brutas que o modelo deu) com mention_labels (rótulos verdadeiros) 
-                #e mede o quanto o modelo errou
+        #loss=erro (compara verdadeiro com as predictions feitas)
+        loss = None #começa com loss vazia (se não houver rótulos, nao precisa calcular nada)
+        if mention_labels is not None:
+            if mention_labels.numel() == 0:
+                loss = logits.new_tensor(0.0)
+            else:
+                loss = torch.nn.functional.binary_cross_entropy_with_logits(
+                    logits, mention_labels.float()
+                ) #binary_cross_entropy_with_logits para comparar logits (notas brutas que o modelo deu) com mention_labels (rótulos verdadeiros) 
+                    #e mede o quanto o modelo errou
 
-    return {"logits": logits, "loss": loss}
+        return {"logits": logits, "loss": loss}
